8b391c17a24a15dd5a4eae4aa9b4d5084248fd44
kahuan <kahuanvitelli@gmail.com>
2025-02-28 15:54:21 -0300

    Atualizado -- Adicionado regra de procura de carteiras

--- api/controllers/customerWallets/searchCustomerWallets.js
+++ api/controllers/customerWallets/searchCustomerWallets.js
@@ -1,8 +1,22 @@
-module.exports= app => {
+module.exports = app => {
     const customerWalletsDB = app.data.customerWallets;
     const controller = {};
 
-    controller.SearchCustomerWallets = (req, res) => res.status(200).json(customerWalletsDB);
-    
+    controller.SearchCustomerWallets = (req, res) => {
+        const Body = req.body;
+
+        if (Object.keys(Body).length === 0 || !Body.id) {
+            return res.status(400).json({ message: "Fail", motive: "Body not exist or incomplete" });
+        }
+
+        const wallet = customerWalletsDB.customerWallets.data.find(wallet => wallet.id === Body.id);
+
+        if (wallet === undefined) {
+            return res.status(200).json({ message: "Success", data: "Any Wallet found" });
+        }
+
+        return res.status(200).json({ message: "Success", data: wallet });
+    };
+
     return controller;
-}
+};
