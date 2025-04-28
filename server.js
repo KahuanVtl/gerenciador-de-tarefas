639702ab75629a585c3d518c99e5b44866624954
kahuan <kahuanvitelli@gmail.com>
2025-04-28 14:22:57 -0300

    Atualizado

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
-const app = require('./config/express')();
+const app = require('./config/express')(); 
 const port = app.get('port');
 
-app.listen(port, () =>{
-    console.log(`servidor rodando na porta ${port}`)
-});
+if (process.env.NODE_ENV !== 'production') {
+  app.listen(port, () => {
+    console.log(`Servidor rodando na porta ${port}`);
+  });
+}
+
+module.exports = app;
